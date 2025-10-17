fc9bdfc5c140a617af86a8b3d1c14b82c9e19b0f
Sahan <sahan.toksoz@student.heaj.be>
2025-10-17 10:42:54 +0200

    SCSS ajout

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,4 @@
-"use strict";
+ "use strict";
 
 
   document.addEventListener("DOMContentLoaded", function () {
@@ -41,4 +41,22 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.utils.toArray(".fade-in-left").forEach((el) => {
+    gsap.to(el, {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      ease: "power2.out",
+      scrollTrigger: {
+        trigger: el,
+        start: "left 85%",
+        toggleActions: "play none none reverse"
+      }
+    });
+  });
+});
 
